411e2226446ee558563f1824b9880bdb44c446c9
Matthew Ohman <ohman@ou.edu>
2020-09-28 13:01:55 -0500

    Need to fix foreach loop

---
+++ (P) PokeApi/(P) PokeApi/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _P__PokeApi
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string ApiLink = @"https://pokeapi.co/api/v2/pokemon/?offset=20&limit=1100";
+
+            Pokemon pkmnAPIResult;
+
+            using (var client = new HttpClient())
+            {
+                string jsonresult = client.GetStringAsync(ApiLink).Result;
+
+                pkmnAPIResult = JsonConvert.DeserializeObject<Pokemon>(jsonresult);
+            }
+            
+
+            foreach (var character in pkmnAPIResult.pkmnlist)
+            {
+                cmbPkmn.Items.Add(character);
+
+                //overide string in class
+            }
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var pkmn = (Pokemon)cmbPkmn.SelectedItem;
+            
+            
+            
+        }
+    }
+}
---
+++ (P) PokeApi/(P) PokeApi/Pokemon.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _P__PokeApi
+{
+    class Pokemon
+    {
+        public List<Results> pkmnlist { get; set; }
+
+        public class Results
+        {
+            public string name { get; set; }
+            public string URLlink { get; set; }
+
+            public override string ToString()
+            {
+                return name;
+            }
+
+            public string CapitalizeFIrstLetter()
+            {
+                return "" + name[0].ToString().ToUpper() + name.Substring(1, name.Length - 2);
+            }
+        }
+
+        public class Stats
+        {
+            public int weight { get; set; }
+            public int height { get; set; }
+        }
+
+        public class Sprite
+        {
+
+        }
+    }
+}
