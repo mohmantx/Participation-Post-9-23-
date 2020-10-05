56e6509e13f95d837d4d49fd3ffb34ee06fe065e
Matthew Ohman <ohman@ou.edu>
2020-10-05 12:14:07 -0500

    Chuck 1

---
+++ Chuck Norris API/Chuck Norris API/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Chuck_Norris_API
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string URLCategory = @"https://api.chucknorris.io/jokes/categories";
+
+            Quotes categories;
+
+            using (var client = new HttpClient())
+            {
+                var json = client.GetStringAsync(URLCategory).Result;
+                categories = JsonConvert.DeserializeObject<Quotes>(json);
+            }
+
+            foreach (var cat in categories.categorylist)
+            {
+                cboCategory.Items.Add(cat);
+            }
+        }
+
+        private void cboCategory_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Chuck Norris API/Chuck Norris API/Quotes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chuck_Norris_API
+{
+    class Quotes
+    {
+        public List<Results> categorylist { get; set; }
+    }
+
+    public class Results
+    {
+        public string categories { get; set; }
+    }
+}
